caa36b284ccc1245ea041e701a5d05c4451913dc
Connor Nolan <connornolan15@gmai.com>
2020-07-28 23:00:21 -0700

    delete default angular html

--- angular-src/src/app/app-routing.module.ts
+++ angular-src/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostCreateComponent } from './components/post-create/post-create.component';
+import { PostComponent } from './components/post/post.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'createpost', component: PostCreateComponent },
+  { path: 'posts', component: PostListComponent },
+  { path: 'post/:id', component: PostComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
