4c1d362b81f0f19feb0cb43654f58d99ea4e6bdb
Connor Nolan <connornolan15@gmai.com>
2020-07-28 22:35:13 -0700

    add post create, list and show components

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PostCreateComponent } from './components/post-create/post-create.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PostCreateComponent,
+    PostListComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
