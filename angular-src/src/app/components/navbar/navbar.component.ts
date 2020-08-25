cb20903255041726e16a78bb4b60263e11d4813f
Connor Nolan <connornolan15@gmai.com>
2020-08-25 02:02:27 -0700

    navbar animates on scroll

--- angular-src/src/app/components/navbar/navbar.component.ts
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import {
     trigger,
     state,
@@ -14,17 +14,16 @@ import {
   animations: [
       trigger('hide', [
           state('visible', style({
-              opacity: 1
+              opacity: 1,
+              height: '4pc'
           })),
           state('hidden', style({
-              opacity: 0.1
+              opacity: 0.1,
+              height: '3pc'
           })),
-          transition('visible => hidden', [
-              animate('0.5s')
-          ]),
-          transition('hidden => visible', [
-              animate('0.5s')
-          ]),
+          transition('visible <=> hidden', [
+              animate('0.3s')
+          ])
       ]),
   ]
 })
@@ -36,14 +35,12 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  toggle() {
-    this.faded = !this.faded;
-    console.log("faded: " + this.faded);
+  @HostListener('window:scroll', ['$event']) onScroll($event) {
+      if (window.pageYOffset > 200) {
+          this.faded = true;
+      }
+      else {
+          this.faded = false;
+      }
   }
-
-  clickFunction(event) {
-      console.log("clicked");
-      this.toggle();
-  }
-
 }
