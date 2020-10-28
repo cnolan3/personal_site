00f81e4cc7e8f90dc2e666815438959af0992a03
Connor Nolan <connornolan15@gmai.com>
2020-10-28 14:00:53 -0700

    add aminated hamburger icon

--- angular-src/src/app/components/navbar/navbar.component.ts
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
 import {
     trigger,
     state,
@@ -9,6 +9,7 @@ import {
     query,
     group
 } from '@angular/animations';
+import { viewClassName } from '@angular/compiler';
 
 @Component({
   selector: 'app-navbar',
@@ -17,8 +18,8 @@ import {
   animations: [
       trigger('expand', [
           state('minimized', style({
-              height: '60px',
-              width: '60px',
+              height: '70px',
+              width: '70px',
           })),
           state('maximized', style({
               height: '300px',
@@ -33,7 +34,7 @@ import {
               query(':enter', [
                 style({ opacity: '0', height: '0px' }),
                 animate('1s', keyframes([
-                  style({ opacity: '0', height: '0px', offset: 0.4 }),
+                  style({ opacity: '0', height: '0px', offset: 0.6 }),
                   style({ opacity: '1', height: '100%', offset: 1 })
                 ]))
               ])
@@ -42,13 +43,13 @@ import {
           transition('maximized => minimized', [
             group([
               animate('1s ease-in-out', keyframes([
-                style({ height: '60px', offset: 0.5 }),
-                style({ width: '60px', offset: 1.0 }),
+                style({ height: '70px', offset: 0.5 }),
+                style({ width: '70px', offset: 1.0 }),
               ])),
               query(':leave', [
                 animate('1s', keyframes([
-                  style({ opacity: '0', offset: 0.5}),
-                  style({ opacity: '0', offset: 1})
+                  style({ opacity: '0', offset: 0.2 }),
+                  style({ opacity: '0', offset: 1 })
                 ]))
               ])
             ])
@@ -61,11 +62,21 @@ export class NavbarComponent implements OnInit {
 
   constructor() { }
   maximized : boolean = false;
+  @ViewChild("menubutton") menuButton;
 
   ngOnInit(): void {
   }
   
   toggle(): void {
       this.maximized = !this.maximized;
+
+      if (this.maximized)
+      {
+        this.menuButton.nativeElement.classList.add("is-active");
+      }
+      else
+      {
+        this.menuButton.nativeElement.classList.remove("is-active");
+      }
   }
 }
