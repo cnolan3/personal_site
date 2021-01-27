4e489fd5d48ace196e1eb6fc1388cd84fed2a4eb
Connor Nolan <connornolan15@gmail.com>
2021-01-26 16:52:39 -0800

    test

--- angular-src/src/app/components/home/home.component.ts
+++ angular-src/src/app/components/home/home.component.ts
@@ -14,6 +14,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.bgImage = 'linear-gradient(0deg, black 0%, rgba(0,0,0,0) 11%), url(' + this.siteApi.getImgUrl("img002.jpg") + ')';
+    console.log(this.siteApi.getImgUrl("img002.jpg"));
   }
 
 }
