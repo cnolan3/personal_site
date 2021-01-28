82f6ea537ae7bf855e6018717520f6249847d818
Connor Nolan <connornolan15@gmail.com>
2021-01-28 00:42:57 -0800

    Update home.component.ts

--- angular-src/src/app/components/home/home.component.ts
+++ angular-src/src/app/components/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { SiteApiService } from '../../services/site-api-service.service';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +7,6 @@ import { SiteApiService } from '../../services/site-api-service.service';
 })
 export class HomeComponent implements OnInit {
 
-  bgImage : string;
-
-  constructor(private siteApi : SiteApiService) { }
-
-  ngOnInit(): void {
-    this.bgImage = 'linear-gradient(0deg, black 0%, rgba(0,0,0,0) 11%), url(' + this.siteApi.getImgUrl("img002.jpg") + ')';
-  }
+  constructor() { }
 
 }
