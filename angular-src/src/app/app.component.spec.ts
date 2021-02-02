80bd2d1846f11b5a6f36077d288c6f7f7f4523e6
Connor Nolan <connornolan15@gmail.com>
2021-02-02 14:28:15 -0800

    dummy commit

--- angular-src/src/app/app.component.spec.ts
+++ angular-src/src/app/app.component.spec.ts
@@ -16,6 +16,7 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
