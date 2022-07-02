926b1ce29855d402edcef6f964e52bfadf813e20
Connor Nolan <connornolandev@gmail.com>
2022-07-02 16:28:39 -0700

    add top level README, add front page component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FrontpageComponent } from './components/frontpage/frontpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FrontpageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
